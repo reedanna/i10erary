69c836c4ee00b6819232afee664ebff5a123fd60
reedanna <reedanna@mail.com>
2020-09-08 14:31:52 -0500

    Added controllers for attractions, days, and destinations, and made seeds file

--- project-backend/app/controllers/attractions_controller.rb
+++ project-backend/app/controllers/attractions_controller.rb
@@ -1,2 +1,27 @@
 class AttractionsController < ApplicationController
+
+    def index
+        attractions = Attraction.all
+        render json: attractions
+    end
+
+    def show
+        attraction = Attraction.find(params[:id])
+        render json: attraction
+    end
+
+    def create
+        attraction = Attraction.new(params)
+        attraction.save
+    end
+
+    def update
+        attraction = Attraction.find(params[:id])
+        attraction.update(params)
+    end
+
+    def destroy
+        attraction = Attraction.find(params[:id])
+        attraction.destroy
+    end
 end
--- project-backend/db/seeds.rb
+++ project-backend/db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do 
+    Destination.create(name: Faker::Address.unique.city, description: Faker::Lorem.paragraphs, image: Faker::Avatar.image)
+    User.create(name: Faker::Internet.unique.username, email: Faker::Internet.unique.email)
+end
+
+30.times do 
+    Attraction.create(destination_id: rand(1..10), name: Faker::Address.unique.community, description: Faker::Lorem.paragraphs, image: Faker::Avatar.image)
+    Trip.create(user_id: rand(1..10), destination_id: rand(1..10), length: 3)
+end
+
+Trip.all.each do |trip|
+    3.times do
+        Day.create(trip_id: trip.id, date: Faker::Date.in_date_period)
+    end
+end
+
+100.times do
+    Visit.create(day_id: rand(1..90), attraction_id: rand(1..30), rating: rand(1..5))
+end
